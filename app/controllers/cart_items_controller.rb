9090ae3ce9acde0c8cbfe05517e772e2f03b8a29
michaelkm <michael@michaelmerrill.io>
2015-06-16 16:33:50 -0600

    Add cart model and controller

---
+++ app/controllers/cart_items_controller.rb
@@ -0,0 +1,15 @@
+class CartItemsController < ApplicationController
+  def create
+    item = Item.find(params[:item_id])
+
+    @cart.add_item(item.id)
+
+    session[:cart] = @cart.contents
+
+    redirect_to cart_items_path
+  end
+
+  def index
+
+  end
+end
