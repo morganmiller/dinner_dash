f2c5825444496ca8be67c306ddf36eea310b5449
Morgan Miller <m.miller722@gmail.com>
2015-06-20 16:52:01 -0600

    add item validations and test

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,6 +3,11 @@ class Item < ActiveRecord::Base
   has_many :categories, through: :item_categories
   has_many :order_items
   has_many :orders, through: :order_items
+  
+  validates :title, presence: true, uniqueness: true
+  validates :description, presence: true
+  validates :price, presence: true, numericality: { greater_than: 0 }
+  validates :categories, presence: true
 
   # scope :category, -> (category) { where category: category }
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -2,4 +2,52 @@ require 'rails_helper'
 
 RSpec.describe Item, type: :model do
   
+  let(:category) { 
+                   Category.create!(name: "Vampire")
+                 }
+  
+  let(:item) {
+                   Item.create!(title: "Monster Blood",
+                                description: "A red, sticky treat.",
+                                price: 15.50,
+                                categories: [category]) 
+             }
+  
+  it "is valid" do
+    expect(item).to be_valid
+  end
+  
+  it "is invalid without a title" do
+    item.title = ""
+    expect(item).to_not be_valid
+  end
+  
+  it "is invalid without a description" do
+    item.description = ""
+    expect(item).to_not be_valid
+  end
+  
+  it "is invalid without a price" do
+    item.price = nil
+    expect(item).to_not be_valid
+  end
+  
+  it "must have decimal price greater than 0" do
+    item.price = 0
+    expect(item.price).to be_a(BigDecimal)
+    expect(item).to_not be_valid
+  end
+  
+  it "must have a unique name" do
+    item
+    expect { Item.create!(title: "Monster Blood",
+                          description: "Another red, sticky treat.",
+                          price: 20.00)
+           }.to raise_error(ActiveRecord::RecordInvalid)
+  end
+  
+  it "must belong to one category minimum" do
+    item.categories = []
+    expect(item).to_not be_valid
+  end
 end
