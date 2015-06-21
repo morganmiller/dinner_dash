ea77c0039ec1eda827aed97e0ae3b34abfdf2253
Morgan Miller <m.miller722@gmail.com>
2015-06-20 22:34:05 -0600

    add validation for items on order and fix controller

--- app/models/order.rb
+++ app/models/order.rb
@@ -4,4 +4,14 @@ class Order < ActiveRecord::Base
   has_many :items, through: :order_items
 
   scope :by_user, ->(user_id) { where(user_id: user_id) }
+  
+  validates :status, presence: true, inclusion: { in: %w(ordered paid cancelled complete), 
+                                                  message: "must be ordered, paid, cancelled or complete."}
+  validate :has_at_least_one_item
+
+  def has_at_least_one_item
+    if items.empty?
+      errors.add(:items, "needs at least one item!")
+    end
+  end
 end
