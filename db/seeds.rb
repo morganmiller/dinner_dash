90a644a2eb86188cf587a9685d821c71996fba34
Morgan Miller <m.miller722@gmail.com>
2015-06-24 13:41:32 -0600

    fix seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -33,10 +33,20 @@ class Seed
   end
 
   def create_orders
-    orders.each do |user_id, status, total_price, items|
-      Order.create(user_id: user_id, status: status, total_price: total_price, items: items)
+    carts.each do |session_cart|
+      cart = Cart.new(session_cart)
+      OrderCreator.execute_order(cart, random_user)
     end
-    puts "Orders: #{Order.all.map(&:user_id).join(", ")} created."
+    Order.find(2).status = "paid"
+    Order.find(4).status = "paid"
+
+    Order.find(6).status = "completed"
+    Order.find(8).status = "completed"
+
+    Order.find(10).status = "cancelled"
+    Order.find(12).status = "cancelled"
+    
+    puts "Orders for the following users: #{Order.all.map(&:user_id).join(", ")} created."
   end
 
   private
@@ -49,19 +59,23 @@ class Seed
       ["Rachel Warbelow","", "demo+rachel@jumpstartlab.com", "password", 0],
     ]
   end
+  
+  def random_user
+    User.find([1,2,3,4].sample)
+  end
 
   def orders
     [
-      [1, "ordered", 1200, [Item.find(1)]],
-      # [2, "completed", 2200],
-      # [3, "cancelled", 400],
-      # [3, "paid", 10300],
-      # [4, "ordered", 2000],
-      # [4, "ordered", 800],
-      # [2, "completed", 1001],
-      # [1, "ordered", 2003],
-      # [3, "cancelled", 300],
-      # [1, "paid", 3000],
+      [1, "ordered", 1200],
+      [2, "completed", 2200],
+      [3, "cancelled", 400],
+      [3, "paid", 10300],
+      [4, "ordered", 2000],
+      [4, "ordered", 800],
+      [2, "completed", 1001],
+      [1, "ordered", 2003],
+      [3, "cancelled", 300],
+      [1, "paid", 3000],
     ]
   end
 
@@ -91,6 +105,24 @@ class Seed
       ["You Can't Scare Me", "Spooky Spooky Spooky", 50.00, [Category.find(4)], "http://i.imgur.com/GOyRZxD.jpg"],
       ["The Girl Who Cried Monster", "Spooky Spooky Spooky", 50.00, [Category.find(4)], "http://i.imgur.com/GOyRZxD.jpg"],
       ["Stay Out Of the Basement", "Spooky Spooky Spooky", 50.00, [Category.find(4)], "http://i.imgur.com/GOyRZxD.jpg"],
+    ]
+  end
+  
+  def carts
+    [
+      { "1" => 2, "3" => 1, "4" => 1 },
+      { "2" => 2, "5" => 1 },
+      { "8" => 1, "6" => 1, "9" => 3 },
+      { "5" => 5 },
+      { "5" => 1, "18" => 1 },
+      { "10" => 1, "12" => 1, "13" => 2, "11" => 3 },
+      { "1" => 4, "9" => 1},
+      { "17" => 2, "2" => 1},
+      { "3" => 1, "4" => 1, "5" => 1, "6" => 1},
+      { "18" => 2},
+      { "7" => 1, "14" => 1, "20" => 4},
+      { "3" => 1 },
+      { "5" => 1, "15" => 5 }
     ]
   end
 end
