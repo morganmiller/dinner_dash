6a9d674d8513d60878536b8a43ec8a5dea81113d
Morgan Miller <m.miller722@gmail.com>
2015-06-24 11:10:04 -0600

    amend test

--- spec/features/user/user_cant_see_admin_pages_spec.rb
+++ spec/features/user/user_cant_see_admin_pages_spec.rb
@@ -1,25 +1,30 @@
 require 'rails_helper'
 
-feature 'Logged in user' do
+feature 'When visiting admin pages' do
   before(:each) do
     @user = User.create!(email: "fml@database.sucks",
       password: "Seekrit",
       full_name: "Sir Whats",
       display_name: "mrwhatsalot")
-
-    @admin = User.create!(email: "admin@example.com",
-      password: "password",
-      full_name: "jeff admin",
-      display_name: "admin",
-      role: 1)
   end
   
-  scenario "can't visit admin page" do
+  scenario "a logged in user is forbidden" do
     allow_any_instance_of(ApplicationController).to receive(:current_user) { @user }
     
     visit admin_items_path
     expect(page.status_code).to eq(403)
     
+    visit new_admin_category_path
+    expect(page.status_code).to eq(403)
+
+    visit admin_orders_path
+    expect(page.status_code).to eq(403)
+  end
+
+  scenario "an unauthenticated user is forbidden" do
+    visit admin_items_path
+    expect(page.status_code).to eq(403)
+
     visit new_admin_category_path
     expect(page.status_code).to eq(403)
 
